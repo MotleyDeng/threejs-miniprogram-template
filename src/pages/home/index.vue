e97a36add56157107050dc04141244908bc98214
邓柯 <210785613@qq.com>
2025-05-06 16:29:57 +0800

    加载模型做成公共组件

---
+++ src/pages/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <LoadModel></LoadModel>
+    </div>
+</template>
+<script setup>
+    import LoadModel from '@/components/LoadModel.vue';
+    import { onMounted } from 'vue';
+    onMounted(() => {
+        console.log('TODO1');
+    });
+</script>
